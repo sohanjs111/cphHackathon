2d11672c5e38158ecc65ef576da779dd68f3892e
Nikita Chernysh <nikitachernysh86@gmail.com>
2025-07-03 12:55:36 +0200

    LETS GOOO

--- job.sh
+++ job.sh
@@ -1,47 +1,47 @@
 #!/bin/bash
 
 ### -- set the job Name -- 
-BSUB -J Nikitas
+#BSUB -J Nikitas
 
 ### -- specify queue -- 
-BSUB -q c02613
+#BSUB -q c02613
 
 ### -- ask for number of cores -- 
-BSUB -n 6
+#BSUB -n 6
 
 ### -- Select the resources: 1 gpu in exclusive process mode --
-BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -gpu "num=1:mode=exclusive_process"
 
 ### -- specify that the cores must be on the same host -- 
-BSUB -R "span[hosts=1]"
+#BSUB -R "span[hosts=1]"
 
 ### -- specify that we need X GB of memory per core/slot -- 
-BSUB -R "rusage[mem=2GB]"
+#BSUB -R "rusage[mem=2GB]"
 
 ### -- set walltime limit: hh:mm or mm--
-BSUB -W 15
+#BSUB -W 100
 
 ### -- set the email address --
-BSUB -u s204165@student.dtu.dk
+#BSUB -u s204165@student.dtu.dk
 ### -- send notification at start --
-BSUB -B
+#BSUB -B
 ### -- send notification at completion--
-BSUB -N
+#BSUB -N
 
 ### -- Specify the output and error file. %J is the job-id -- 
 ### -- -o and -e mean append, -oo and -eo mean overwrite -- 
-BSUB -o Output_%J.out 
-BSUB -e Output_%J.err 
+#BSUB -o Output_%J.out 
+#BSUB -e Output_%J.err 
 
-module load cuda/11.8 
-module load matplotlib/3.10.0-numpy-2.2.2-python-3.10.16
-module load numpy/2.2.2-python-3.10.16-openblas-0.3.29
-module load pandas/2.2.3-python-3.10.16
-module load scipy/1.15.1-python-3.10.16
+#module load cuda/11.8 
+#module load matplotlib/3.10.0-numpy-2.2.2-python-3.10.16
+#module load numpy/2.2.2-python-3.10.16-openblas-0.3.29
+#module load pandas/2.2.3-python-3.10.16
+#module load scipy/1.15.1-python-3.10.16
 module load nccl/2.21.5-1-cuda-12.5 
 
 # Setup env
 source /dtu/blackhole/00/best/env_vlm_bk/bin/activate
 
 # the file to run 
-python pipeline.py
+python milestone_hackathon.py
